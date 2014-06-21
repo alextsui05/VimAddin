f5d5a2b495ef821d0b74212de5ffc86dcb7126cb
Alex Tsui <atsui@ucdavis.edu>
2014-06-21 15:55:16 -0700

    Initial import.

---
+++ VimAddin/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"VimAddin", 
+	Namespace = "VimAddin",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("VimAddin")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("A fork of the core vi modes in MonoDevelop.")]
+[assembly:AddinAuthor ("atsui")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
---
+++ VimAddin/VimTextEditorExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MonoDevelop.Ide.Gui.Content;
+
+namespace VimAddin
+{
+	public class VimTextEditorExtension : TextEditorExtension
+	{
+		public VimTextEditorExtension ()
+		{
+
+		}
+
+		public override void Initialize( )
+		{
+			base.Initialize ();
+			Console.WriteLine ("VimAddin.VimTextEditorExtension.Initialize");
+		}
+	}
+}
