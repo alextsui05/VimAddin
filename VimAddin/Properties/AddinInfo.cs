559f43e3d9d545564a946252e95499dc3abba4aa
Alex Tsui <atsui@ucdavis.edu>
2014-08-05 09:48:49 -0700

    Increment addin version and update addin description.

--- VimAddin/Properties/AddinInfo.cs
+++ VimAddin/Properties/AddinInfo.cs
@@ -5,12 +5,12 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"VimAddin", 
 	Namespace = "VimAddin",
-	Version = "1.1.5"
+	Version = "1.1.7"
 )]
 
 [assembly:AddinName ("VimAddin")]
 [assembly:AddinCategory ("IDE extensions")]
-[assembly:AddinDescription ("A fork of the core vi modes in MonoDevelop.")]
+[assembly:AddinDescription ("A fork of the core vi modes in MonoDevelop. See https://github.com/alextsui05/VimAddin")]
 [assembly:AddinAuthor ("atsui")]
 
 [assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
